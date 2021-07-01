8e45ec6e093cc25e7a8524106588bbc2f4190b49
ChrisNg02655332 <chris.dungnguyen@gmail.com>
2021-07-01 17:06:00 +0700

    chore: update example

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -48,16 +48,21 @@ export default function App() {
 
   return (
     <SafeAreaView style={{ flex: 1 }}>
+      <Text style={styles.title}>React Native Form Builder</Text>
       <FlatList
         horizontal
         style={styles.list}
         data={data}
         renderItem={({ item, index }) => (
           <TouchableOpacity
-            style={styles.btn}
+            style={[styles.btn, index === currentView && styles.activeBtn]}
             onPress={() => setCurrentView(index)}
           >
-            <Text style={styles.text}>{item.title}</Text>
+            <Text
+              style={[styles.text, index === currentView && styles.activeText]}
+            >
+              {item.title}
+            </Text>
           </TouchableOpacity>
         )}
         ItemSeparatorComponent={() => <View style={{ width: 15 }} />}
@@ -70,6 +75,14 @@ export default function App() {
 }
 
 const styles = StyleSheet.create({
+  title: {
+    marginTop: 20,
+    marginBottom: 30,
+    fontSize: 30,
+    textAlign: 'center',
+    fontWeight: 'bold',
+    color: '#0284C7',
+  },
   list: {
     maxHeight: 55,
     marginHorizontal: 10,
@@ -86,5 +99,12 @@ const styles = StyleSheet.create({
   },
   text: {
     fontWeight: '600',
+  },
+  activeBtn: {
+    backgroundColor: '#60A5FA',
+  },
+  activeText: {
+    color: 'white',
+    fontWeight: 'bold',
   },
 })
