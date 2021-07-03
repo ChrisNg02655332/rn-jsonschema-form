3ee914eba2aec0372c2794fdcb35a6674d0c2cbb
ChrisNg02655332 <chris.dungnguyen@gmail.com>
2021-07-03 23:39:55 +0700

    feat: apply gh-pages

---
+++ gh-pages/.next/static/6zSqZDyxlkpQybNAGgev3/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-afbcda88da9712a0a525.js"],"/_error":["static/chunks/pages/_error-8842bdd603648886795b.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
