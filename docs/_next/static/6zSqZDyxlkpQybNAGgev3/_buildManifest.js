7120cdc8af2e820da7cd856ec6487939123301be
ChrisNg02655332 <chris.dungnguyen@gmail.com>
2021-07-04 19:20:21 +0700

    feat: revert

--- docs/_next/static/6zSqZDyxlkpQybNAGgev3/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-afbcda88da9712a0a525.js"],"/_error":["static/chunks/pages/_error-8842bdd603648886795b.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
