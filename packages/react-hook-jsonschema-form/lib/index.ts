754095f06c5739e9d022acb6326219203ca49886
thainh93 <hongthainguyen.2001@gmail.com>
2023-02-27 21:51:28 +0700

    feat: correct import type

--- packages/react-hook-jsonschema-form/lib/index.ts
+++ packages/react-hook-jsonschema-form/lib/index.ts
@@ -1,3 +1,4 @@
-import { getWidget, getUiOptions, Methods } from 'jsonshema-form-core'
+import { getWidget, getUiOptions } from 'jsonshema-form-core'
+import type { Methods } from 'jsonshema-form-core'
 export * from './components/Form'
 export { getWidget, getUiOptions, Methods }
