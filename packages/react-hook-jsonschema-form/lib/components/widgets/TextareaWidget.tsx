fe0d1552b90e0e17eeef337d09030c32a8f936d8
thainh93 <hongthainguyen.2001@gmail.com>
2023-04-27 09:54:41 +0700

    feat: update textarea widget

--- packages/react-hook-jsonschema-form/lib/components/widgets/TextareaWidget.tsx
+++ packages/react-hook-jsonschema-form/lib/components/widgets/TextareaWidget.tsx
@@ -2,14 +2,14 @@ import { CommonProps } from 'jsonshema-form-core'
 
 export { TextareaWidget }
 
-function TextareaWidget({ name, required, methods, schema, placeholder, disabled }: CommonProps) {
+function TextareaWidget({ name, required, methods, schema, uiSchema, disabled }: CommonProps) {
   const { register } = methods
   const fieldName = schema?.parentKey ? `${schema?.parentKey}.${name}` : `${name}`
 
   return (
     <textarea
       className="form-control"
-      placeholder={placeholder}
+      placeholder={uiSchema?.['ui:placeholder'] || ''}
       {...register(fieldName, { required, disabled })}
     ></textarea>
   )
